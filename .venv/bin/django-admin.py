334512896981a8d382727def18320527c8d2c664
babakmhz <babakmhz96@gmail.com>
2019-06-05 15:18:45 +0430

    first commit

---
+++ .venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/babakmhz/0_stuff/0_programming/python-projects/cafe_fanus/.venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
